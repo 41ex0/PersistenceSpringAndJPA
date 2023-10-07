29e3aac0c6e0ac4946e21baf16d816ebc86fbfc7
AlexisCepeda <alexisnatel@gmail.com>
2023-10-07 14:25:29 -0300

    spring initializr base

---
+++ src/main/java/Spring3/ElBuenSabor/ElBuenSaborApplication.java
@@ -0,0 +1,13 @@
+package Spring3.ElBuenSabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElBuenSaborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElBuenSaborApplication.class, args);
+	}
+
+}
